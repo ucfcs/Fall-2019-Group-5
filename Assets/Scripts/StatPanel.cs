23a4bc305e7088a771819386a61a6593ade144c2
comcbryde <42752512+comcbryde@users.noreply.github.com>
2019-11-20 14:16:31 -0500

    Adding Tooltip code
    Hopefully doesn't break

---
+++ Assets/Scripts/StatPanel.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatPanel : MonoBehaviour
+{
+    [Header("Link")]
+    public StatManager lManager;
+    public renameObjectClass lEvidence;
+
+    [Header("Values")]
+    public string vName;
+    public string vInfo;
+
+    [Header("Containers")]
+    public Text cInfo;
+
+    public void Initialize(StatManager manage, renameObjectClass evidence, string name, string info)
+    {
+        lManager = manage;
+        lEvidence = evidence;
+        vName = name;
+        vInfo = info;
+
+
+
+        
+
+        Locate();
+        return;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("SlowUpdate", 0f, Data.SlowUpdateRate / 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Locate();
+    }
+
+    
+
+    void Locate()
+    {
+        Vector3 distanceBonus = lManager.panelDistanceBonus;
+        Camera cam = FindObjectOfType<Camera>();
+        float DistanceToCamera = (this.transform.position - cam.transform.position).magnitude;
+
+        switch (lManager.panelDistanceFunction)
+        {
+            case Data.MathFunction.Add:
+                distanceBonus = distanceBonus.normalized
+                    * (distanceBonus.magnitude + lManager.panelDistanceFactor * DistanceToCamera);
+                break;
+            case Data.MathFunction.Subtract:
+                distanceBonus = distanceBonus.normalized
+                    * (distanceBonus.magnitude - lManager.panelDistanceFactor * DistanceToCamera);
+                break;
+            case Data.MathFunction.Multiply:
+                distanceBonus = distanceBonus.normalized
+                    * (distanceBonus.magnitude * lManager.panelDistanceFactor * DistanceToCamera);
+                break;
+            case Data.MathFunction.Divide:
+                distanceBonus = distanceBonus.normalized
+                    * (distanceBonus.magnitude / (lManager.panelDistanceFactor * DistanceToCamera));
+                break;
+        }
+
+        this.transform.position = lEvidence.transform.position + lManager.panelOffset + distanceBonus;
+        return;
+    }
+
+
+
+}
---
+++ Assets/Scripts/renameObjectClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class renameObjectClass : MonoBehaviour
+{
+    //	Object DATA
+    [Header("Object Data")]
+    //	Data for object
+    [Tooltip("Info Data")]
+    public string oInfo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
