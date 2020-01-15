fe628d7171f24fb20b5ffce2b2dba92531854709
comcbryde <42752512+comcbryde@users.noreply.github.com>
2020-01-15 17:36:32 -0500

    Should work, works on my computer at least.
    Both my video loading section and the map selection worked on my end, so this should work.

--- Assets/Scripts/FileLoad.cs
+++ Assets/Scripts/FileLoad.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class FileLoad : MonoBehaviour
 {
+    public Text ArtifactFileName;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +24,34 @@ public class FileLoad : MonoBehaviour
 
         Data.MapFileLoc = EditorUtility.OpenFilePanel("File Browser", "", "");
 
+    }
+
+    public void LoadVideoFile()
+    {
+
+        Data.CurrentVideoFileLoc = EditorUtility.OpenFilePanel("Select a Video", "", "mp4");
+        Text my_text = GameObject.Find("Video Title").GetComponent<Text>();
+        my_text.text = "Current Video: \n" + System.IO.Path.GetFileName(Data.CurrentVideoFileLoc);
+
+    }
+
+    public void LoadArtifactFile()
+    {
+        if(Data.CurrentVideoFileLoc != "")
+        {
+            Data.CurrentArtifactFileLoc = EditorUtility.OpenFilePanel("Select an image", "", "");
+            Text txt = GameObject.Find("ArtifactFileName").GetComponent<Text>();
+            if(Data.CurrentArtifactFileLoc != "")
+            {
+                txt.text = "Current Artifact: " + System.IO.Path.GetFileName(Data.CurrentArtifactFileLoc);
+            }
+            else
+            {
+                txt.text = "Select a File";
+            }
+            
+        }
+
+        
     }
 }
