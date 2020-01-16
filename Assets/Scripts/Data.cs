b7520eb5c2b845916cb7be2ea61996afeb1c18a8
comcbryde <42752512+comcbryde@users.noreply.github.com>
2020-01-16 08:04:56 -0500

    Added my section to the main scene.
    More specifically, added artifact loading and video loading to FileLoad, and added the Select File button, Load Video button, Save Artifacts to Video button, Save Artifact button, and the Supporting/Irrelevant/Refuting dropdown alongside the timestamp section.

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -8,7 +8,12 @@ public class Data : MonoBehaviour
 
     public static Sprite MapTexture = null;
     public static string MapFileLoc = "";
-    public static ArrayList VideoFileLoc;
+    public static string CurrentVideoFileLoc = "";
+    public static string CurrentArtifactFileLoc = "";
+    //For each list in the List, the first object will be the video file location, while the other objects will be represented by
+    //Strings of the format: "imageLocation, timestamp, Supporting/Refuting/Irrelevant Num" 
+    //As an example: "D:/Picture/pic1.png, 00:01, 1"
+    public static List<List<string>> VideoFileAndArtifactLocs;
 
     public enum MathFunction
     {
